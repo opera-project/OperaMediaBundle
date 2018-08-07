2c316b208f6934293f53b8cd0694bef23989f59e
Cedric Lombardot <cedric.lombardot@gmail.com>
2018-08-07 16:35:43 +0200

    Init OperaMediaBundle

---
+++ OperaMediaBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Opera\MediaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class OperaMediaBundle extends Bundle
+{
+}
