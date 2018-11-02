378968fc529527723fd9d5206a412245f86f4aec
Cedric Lombardot <cedric.lombardot@gmail.com>
2018-11-02 16:55:02 +0100

    Fix media picker

--- Form/MediaEntityType.php
+++ Form/MediaEntityType.php
@@ -8,8 +8,10 @@ use Opera\MediaBundle\Entity\Media;
 use Opera\MediaBundle\MediaManager\SourceManager;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Opera\CoreBundle\Form\DataTransformer\IdToModelTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class MediaEntityType extends AbstractType
 {
@@ -28,23 +30,27 @@ class MediaEntityType extends AbstractType
         $sources = $this->sourceManager->getSources();
 
         $resolver->setDefaults([
-            'class' => Media::class,
             'sources' => $sources,
             'selected_source' => array_values($sources)[0],
             'selected_folder' => null,
             'pagerFantaMedia' => array_values($sources)[0]->listMedias(null, 1),
-            'folders' => null,
+            'folders' => array_values($sources)[0]->listFolders(),
         ]);
     }
 
     public function getParent()
     {
-        return EntityType::class;
+        return TextType::class;
     }
 
     public function getBlockPrefix()
     {
         return 'media_entity';
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new IdToModelTransformer($this->registry, Media::class));
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options)
