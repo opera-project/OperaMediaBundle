ec9a4a49fc1b2022ee5a29abac5e254679e5a37f
Nsy <noelie@donkeycode.com>
2019-11-29 12:30:59 +0100

    fix pagination ajax in modal

--- Resources/public/opera_media_picker/media-picker.js
+++ Resources/public/opera_media_picker/media-picker.js
@@ -3,6 +3,9 @@ var selectMedia = function (FormVarsId) {
 
     $(document).on('click', '#mediaPickerModal' + FormVarsId + ' [data-select-media]', function (event) {
         event.preventDefault();
+        /**
+         * Prevent "click=>refresh/change page" when selecting the media
+         */
 
         $("#" + FormVarsId).val($(this).data('select-media'));
         $("#" + FormVarsId).parent().find('img').attr('src', $(this).data('select-media-preview'));
@@ -22,11 +25,15 @@ var selectMedia = function (FormVarsId) {
 var ajaxMediaLink = function (FormVarsId, callback = null) {
     $(document).on('click', '#mediaPickerModal' + FormVarsId + ' [data-ajax-link]', function (event) {
         event.preventDefault();
+        /**
+         * Prevent the "click=>refresh/change page" when changing folder
+         */
 
         $.ajax({
             url: $(this).attr('href'),
             success: function (data) {
                 $(document).find('.modal-body').html($(data).find('#main').html());
+                addDataAjaxLinkToPagination();
                 if (callback) {
                     callback();
                 }
@@ -39,6 +46,9 @@ var ajaxMediaLink = function (FormVarsId, callback = null) {
 var preventDefaultSubmit = function (FormVarsId, callback = null) {
     $(document).on('submit', '#mediaPickerModal' + FormVarsId + ' .modal-body form', function (event) {
         event.preventDefault();
+        /**
+         * Prevent the "click=>refresh/change page" when creating a folder or media
+         */
 
         var options = {
             url: event.currentTarget.action,
@@ -62,4 +72,10 @@ var preventDefaultSubmit = function (FormVarsId, callback = null) {
 
         $.ajax(options);
     });
+}
+
+var addDataAjaxLinkToPagination = function () {
+     $('.pagination a').each(function(e) {
+        $(this).attr('data-ajax-link', '');
+    })
 }
